52fecf051d78c6130f1965a71f139300acbcae8d
Kim North <mickeymirage@hotmail.com>
2018-05-09 23:39:15 +0100

    Adding test for checking if card has character face. Adding instance method with default of 'nil' if not in instantiation.

--- specs/specs.rb
+++ specs/specs.rb
@@ -4,7 +4,8 @@ require '../app/card'
 class CardTest < Minitest::Test
 
 	def setup
-		@test_card = Card.new(10)
+		@test_card = Card.new(10, :clubs)
+		@test_card2 = Card.new(10, :clubs, :king)
 	end
 
 	def test_card_exists
@@ -14,6 +15,18 @@ class CardTest < Minitest::Test
 	def test_card_has_value
 		expected = 10
 		actual = @test_card.face_value
+		assert_equal(expected, actual)
+	end
+
+	def test_card_has_suit
+		expected = :clubs
+		actual = @test_card.suit
+		assert_equal(expected, actual)
+	end
+
+	def test_card_has_face
+		expected = :king
+		actual = @test_card2.face_character
 		assert_equal(expected, actual)
 	end
 
