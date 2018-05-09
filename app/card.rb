36486bd929df74b5185af2800cd6af1e634c69a8
Kim North <mickeymirage@hotmail.com>
2018-05-09 23:51:49 +0100

    Adding test to check for existence of Deck class. Creating deck class ruby file and adding a basic class to it.

--- app/card.rb
+++ app/card.rb
@@ -1,3 +1,11 @@
 class Card
 
+	attr_reader :face_value, :suit, :face_character
+
+	def initialize(face_value, suit, face_character=nil)
+		@face_value = face_value
+		@suit = suit
+		@face_character = face_character
+	end
+
 end
--- specs/specs.rb
+++ specs/specs.rb
@@ -1,12 +1,16 @@
 require 'minitest/autorun'
 require '../app/card'
+require '../app/deck'
 
 class CardTest < Minitest::Test
 
 	def setup
 		@test_card = Card.new(10, :clubs)
 		@test_card2 = Card.new(10, :clubs, :king)
+		@test_deck = Deck.new()
 	end
+
+	# Card Class
 
 	def test_card_exists
 		assert_instance_of Card, @test_card
@@ -28,6 +32,12 @@ class CardTest < Minitest::Test
 		expected = :king
 		actual = @test_card2.face_character
 		assert_equal(expected, actual)
+	end
+
+	# Deck Class
+
+	def test_deck_exists
+		assert_instance_of Deck, @test_deck
 	end
 
 end
